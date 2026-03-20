2d5f2a0e5f09944a53be22ed353ba08ae495b80b
chitha_srinath <chithasaireddy@gmail.com>
2026-03-20 20:09:41 +0530

    feat: implement Axios instance with request and response interceptors for API communication and error handling.

--- src/apiService/axiosInstance.js
+++ src/apiService/axiosInstance.js
@@ -23,28 +23,28 @@ axiosInstance.interceptors.request.use(
 );
 
 // Response interceptor
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     if (response.data.statuscode === 401 || response.data.statuscode === 400) {
-//       console.error("authentication failed");
-//       window.location.href = "/login";
-//       return Promise.reject(response);
-//     } else if (response.data.statuscode === 409) {
-//       console.error("user register failed");
-//       window.location.href = "/register";
-//       return Promise.reject(response);
-//     } else if (response.data.statuscode === 500) {
-//       console.error("Server error, please try again later.");
-//       return Promise.reject(response);
-//     }
-//     if (response.data.success && response.data.statuscode === 200) {
-//       return response;
-//     }
-//     return Promise.reject("something went wrong, try again later");
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.response.use(
+  (response) => {
+    if (response.data.statuscode === 401 || response.data.statuscode === 400) {
+      console.error("authentication failed");
+      window.location.href = "/login";
+      return Promise.reject(response);
+    } else if (response.data.statuscode === 409) {
+      console.error("user register failed");
+      window.location.href = "/register";
+      return Promise.reject(response);
+    } else if (response.data.statuscode === 500) {
+      console.error("Server error, please try again later.");
+      return Promise.reject(response);
+    }
+    if (response.data.success && response.data.statuscode === 200) {
+      return response;
+    }
+    return Promise.reject("something went wrong, try again later");
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
